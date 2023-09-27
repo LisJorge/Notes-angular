eebac576de7316d3f1d10f5a045fb95b6e460c10
LisJorge <daris10jorge@gmail.com>
2023-09-27 15:05:31 -0600

    ✨ Add new notes

--- src/notes-form/notes-form.component.ts
+++ src/notes-form/notes-form.component.ts
@@ -1,23 +1,39 @@
 import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Notes } from 'src/notes-list/sample-data';
 
 @Component({
   selector: 'app-notes-form',
   standalone: true,
   templateUrl: './notes-form.component.html',
   styleUrls: ['./notes-form.component.css', './../app/app.component.css'],
-  imports: [ FormsModule, NgIf]
+  imports: [ FormsModule, NgIf, ReactiveFormsModule]
 })
 export class NotesFormComponent {
-  noteTitle?: string;
-  noteDescription?: string;
+  newNoteForm = new FormGroup({
+    noteTitle: new FormControl('', Validators.required),
+    noteDescription: new FormControl('', Validators.required),
+  });
 
   submit(){
-    const newNote = {
-      title: this.noteTitle,
-      description: this.noteDescription,
+    const newNoteData = {
+      title:  this.newNoteForm.value.noteTitle,
+      description: this.newNoteForm.value.noteDescription,
     }
-    alert(newNote)
+    if (this.newNoteForm.valid) {
+      const ids = Notes.map((note) => note.id);
+      let maxId = 0;
+      if (ids.length > 0) {
+        maxId = Math.max(...ids);
+      }
+      let newNote = {
+        id: maxId + 1,
+        title: newNoteData.title!,
+        text: newNoteData.description!,
+      };
+      Notes.unshift(newNote);
+      this.newNoteForm.reset();
+    }
   }
 }
