c7eae87ee6f3f0903b73a7118dc37313adc6cae5
LisJorge <daris10jorge@gmail.com>
2023-09-29 12:32:34 -0600

    🎨 Fix css for adding details button

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,13 @@ import { AppComponent } from './app/app.component';
 import { Routes, provideRouter } from '@angular/router';
 import { NotesFormComponent } from './notes-form/notes-form.component';
 import { NotesListComponent } from './notes-list/notes-list.component';
+import { NoteDetailsComponent } from './note-details/note-details.component';
 
 const routes: Routes = [
     { path: '', component: NotesListComponent },
     { path: 'new', component: NotesFormComponent },
+    { path: 'note', component: NoteDetailsComponent },
+    //{ path: '**', title: 'Not found' },
   ]
 
 bootstrapApplication(AppComponent,{
---
+++ src/note-details/note-details.component.ts
@@ -0,0 +1,17 @@
+import { NgIf } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { Notes } from 'src/notes-list/sample-data';
+
+@Component({
+  selector: 'app-note-details',
+  standalone: true,
+  templateUrl: './note-details.component.html',
+  styleUrls: ['/./../app/app.component.css'],
+  imports: [RouterModule, NgIf],
+})
+export class NoteDetailsComponent {
+  activeRoute = inject(ActivatedRoute);
+  id = Number(this.activeRoute.snapshot.paramMap.get('id'));
+  note = Notes.find((note) => note.id === this.id);
+}
